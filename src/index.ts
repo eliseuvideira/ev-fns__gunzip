1a69b2b1b2e4ee5efb44f3db939adf9706aec076
Eliseu Videira <eliseuvideira1@gmail.com>
2021-04-25 17:49:53 -0300

    :sparkles: done so far

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World 👋");
+import zlib from "zlib";
+import fs from "fs";
+import { pipe } from "@ev-fns/pipe";
+
+export const gunzip = async (
+  input: string,
+  output = /\.gz$/.test(input) ? input.replace(/\.gz$/, "") : input + ".gunzip"
+) => {
+  const gunzipper = zlib.createGunzip();
+  const source = fs.createReadStream(input);
+  const destination = fs.createWriteStream(output);
+  await pipe(source, gunzipper, destination);
+  await fs.promises.unlink(input);
+};
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,16 +1,81 @@
-describe("index", () => {
-  it("greets with Hello World!", () => {
+import fs from "fs";
+import path from "path";
+import { gunzip } from "../src";
+import { gzip } from "@ev-fns/gzip";
+
+describe("gunzip", () => {
+  it("gunzips a file", async () => {
     expect.assertions(3);
-    const log = jest.fn();
-    const _ = console.log;
-    console.log = log;
+
+    const content = "file content".repeat(5);
+
+    const filename = path.join(__dirname, Date.now().toString());
+    const gzipFilename = filename + ".gz";
+
+    await fs.promises.writeFile(filename, content);
+
+    const fileStats = await fs.promises.stat(filename);
+
+    await gzip(filename, gzipFilename);
+
+    const gzipStats = await fs.promises.stat(gzipFilename);
+
+    await gunzip(gzipFilename, filename);
+
+    const gunzipStats = await fs.promises.stat(filename);
+
+    const gunzipContent = await (await fs.promises.readFile(filename)).toString(
+      "utf8"
+    );
+
     try {
-      require('../src/index.ts');
-      expect(log).toHaveBeenCalled();
-      expect(log).toHaveBeenCalledTimes(1);
-      expect(log).toHaveBeenCalledWith("Hello World 👋");
+      expect(gunzipStats.size).toBeGreaterThan(gzipStats.size);
+      expect(gunzipStats.size).toBe(fileStats.size);
+      expect(gunzipContent).toBe(content);
     } finally {
-      console.log = _;
+      await fs.promises.unlink(filename);
     }
+  });
+
+  it("renames output file to remove .gz", async () => {
+    expect.assertions(1);
+    const filename = path.join(__dirname, Date.now().toString());
+    const gzipFilename = filename + ".gz";
+    await fs.promises.writeFile(filename, "a".repeat(50));
+
+    await gzip(filename, gzipFilename);
+
+    await gunzip(gzipFilename);
+
+    let gunzipStats = null;
+    try {
+      gunzipStats = await fs.promises.stat(filename);
+      expect(gunzipStats).toBeDefined();
+    } catch (err) {
+      fail(err);
+    }
+
+    await fs.promises.unlink(filename);
+  });
+
+  it("renames output file as .gunzip if no .gz", async () => {
+    expect.assertions(1);
+    const filename = path.join(__dirname, Date.now().toString());
+    const gzipFilename = filename + ".any";
+    await fs.promises.writeFile(filename, "a".repeat(50));
+
+    await gzip(filename, gzipFilename);
+
+    await gunzip(gzipFilename);
+
+    let gunzipStats = null;
+    try {
+      gunzipStats = await fs.promises.stat(gzipFilename + ".gunzip");
+      expect(gunzipStats).toBeDefined();
+    } catch (err) {
+      fail(err);
+    }
+
+    await fs.promises.unlink(gzipFilename + ".gunzip");
   });
 });
